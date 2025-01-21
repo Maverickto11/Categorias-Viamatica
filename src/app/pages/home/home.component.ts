d042d11b96105d58101e2430a7a3dc8126e9b04c
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2025-01-21 18:25:03 -0500

    .

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -27,7 +27,7 @@ export class HomeComponent implements OnInit{
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.categoriaId = +params['id'];  
-      this.obtenerPublicaciones();  
+      this.obtenerTodasPublicaciones();  
     });
   }
 
